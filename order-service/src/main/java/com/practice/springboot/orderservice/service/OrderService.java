38cba9d7ed5042ce7c9a19f9d443dd2f2d79002c
kaustubh0201 <kaustubh1818@gmail.com>
2022-12-05 20:09:48 +0530

    chore: added discovery-server; services made as client-server; api calling successfully;

--- order-service/src/main/java/com/practice/springboot/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/practice/springboot/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
 
@@ -43,8 +43,8 @@ public class OrderService {
                 .toList();
 
         // call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
